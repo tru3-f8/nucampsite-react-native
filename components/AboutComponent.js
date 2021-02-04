8099c94c765dcdcdd9b19e7b9c11c45a93bf6013
Johnny <jliang61987@gmail.com>
2021-02-03 16:57:27 -0800

    react-native

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -5,6 +5,7 @@ import { FlatList } from 'react-native-gesture-handler';
 import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
   return {
@@ -44,6 +45,29 @@ class About extends Component {
         />
       )
     }
+
+    if (this.props.partners.isLoading) {
+      return (
+          <ScrollView>
+              <Mission />
+              <Card
+                  title='Community Partners'>
+                  <Loading />
+              </Card>
+          </ScrollView>
+      );
+  }
+  if (this.props.partners.errMess) {
+      return (
+          <ScrollView>
+              <Mission />
+              <Card
+                  title='Community Partners'>
+                  <Text>{this.props.partners.errMess}</Text>
+              </Card>
+          </ScrollView>
+      );
+  }
 
     return (
       <ScrollView>
