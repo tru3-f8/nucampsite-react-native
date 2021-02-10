5631287a47e5acd0b8e7e2a86605f10d10aca0b0
Johnny <jliang61987@gmail.com>
2021-02-09 16:47:26 -0800

    react-native

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -6,6 +6,7 @@ import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import Loading from './LoadingComponent';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
   return {
@@ -49,22 +50,26 @@ class About extends Component {
     if (this.props.partners.isLoading) {
       return (
           <ScrollView>
+              <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
               <Mission />
               <Card
                   title='Community Partners'>
                   <Loading />
               </Card>
+              </Animatable.View>
           </ScrollView>
       );
   }
   if (this.props.partners.errMess) {
       return (
           <ScrollView>
+            <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
               <Mission />
               <Card
                   title='Community Partners'>
                   <Text>{this.props.partners.errMess}</Text>
               </Card>
+              </Animatable.View>
           </ScrollView>
       );
   }
--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -4,6 +4,7 @@ import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import { postFavorite, postComment } from '../redux/ActionCreators';
+import * as Animatable from 'react-native-animatable';
 
 
 const mapStateToProps = state => {
@@ -26,6 +27,7 @@ function RenderCampsite(props) {
 
     if (campsite) {
         return (
+            <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
             <Card
                 featuredTitle={campsite.name}
                 image={{uri: baseUrl + campsite.image}}>
@@ -50,6 +52,7 @@ function RenderCampsite(props) {
                     />
                 </View>
             </Card>
+            </Animatable.View>
         )
     }
     return <View />;
@@ -71,6 +74,7 @@ function RenderComments({comments}) {
     }
 
     return (
+        <Animatable.View animation='fadeInUp' duration={2000} delay={1000}>
         <Card title="Comments">
             <FlatList
             data={comments}
@@ -78,6 +82,7 @@ function RenderComments({comments}) {
             keyExtractor={item => item.id.toString()}
             />
         </Card>
+        </Animatable.View>
     ) 
 }
 
