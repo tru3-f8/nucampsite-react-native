471cafc085c9bd77b4bf4fd575b3852e487aefdf
Johnny <jliang61987@gmail.com>
2021-02-20 13:20:42 -0800

    react-native

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -339,20 +339,35 @@ class Main extends Component {
         this.props.fetchPromotions();
         this.props.fetchPartners();
 
-        NetInfo.fetch().then(connectionInfo => {
-            (Platform.OS === 'ios')
-                ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
-                : ToastAndroid.show('Initial Network Connectivity Type: ' +
-                    connectionInfo.type, ToastAndroid.LONG);
-        });
+        // NetInfo.fetch().then(connectionInfo => {
+        //     (Platform.OS === 'ios')
+        //         ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
+        //         : ToastAndroid.show('Initial Network Connectivity Type: ' +
+        //             connectionInfo.type, ToastAndroid.LONG);
+        // });
 
-        this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
-            this.handleConnectivityChange(connectionInfo);
-        });
+        //   this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
+        //     this.handleConnectivityChange(connectionInfo);
+        // });
+
+        showNetInfo = async () => {
+            const NetInfo = await fetch(connectionInfo); 
+                (Platform.OS === 'ios')
+                    ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
+                    : ToastAndroid.show('Initial Network Connectivity Type: ' +
+                        connectionInfo.type, ToastAndroid.LONG);
+            };
+    
+              this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
+                this.handleConnectivityChange(connectionInfo);
+            });
+    
     }
 
     componentWillUnmount() {
         this.unsubscribeNetInfo();
+
+    
     }
     
     handleConnectivityChange = connectionInfo => {
